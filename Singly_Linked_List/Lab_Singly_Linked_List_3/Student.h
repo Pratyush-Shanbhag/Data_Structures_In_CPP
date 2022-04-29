76c531ac93e0d1248c99c5b1992fb3c854cad6c2
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-04-28 23:44:26 -0700

    added skeleton code

---
+++ Singly_Linked_List/Lab_Singly_Linked_List_3/Student.h
@@ -0,0 +1,29 @@
+// Specification file for the Student class
+// Modified by:
+// IDE:
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+//using namespace std;  //<==== This statement
+// in a header file of a complex project could create
+// namespace management problems for the entire project
+// (such as name collisions).
+// Do not write namespace using statements at the top level in a header file!
+
+using std::string;
+
+class Student
+{
+private:
+/* Write your code here: gpa - a double, name - a string */
+
+    
+public:
+    /* Write your code here: default and overloaded constructors  */
+    
+    // Setters and getters
+    /* Write your code here: a setter and a getter for each data member of the class  */
+    
+};
+#endif

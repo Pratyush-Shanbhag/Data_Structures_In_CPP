6ceee18a05bc60672868452e0232f6177482b053
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2022-07-21 21:23:04 +0530

    progress

---
+++ Doubly_Linked_List/Lab__ Doubly-Linked Lists - Templates /College.h
@@ -0,0 +1,52 @@
+// Specification file for the College class
+// Written By: A. Student
+// Changed By: Pratyush Shanbhag
+// IDE: VSCode
+
+#ifndef COLLEGE_H
+#define COLLEGE_H
+
+#include<string>
+
+using std::string;
+
+class College
+{
+    private:
+        string code;
+        string name;
+        int rank;
+        int cost;
+
+    public:
+        //constructors
+        College();
+        College(int, string, string, int);
+
+        //setters
+        void setCode(string cd){ code = cd;}
+        void setName(string nm){ name = nm;}
+        void setRank(int rk){ rank = rk;}
+        void setCost(int cs){ cost = cs;}
+
+        //getters
+        string getCode() const {return code;}
+        string getName() const {return name;}
+        int getRank() const {return rank;}
+        int getCost() const {return cost;}
+
+        //other functions
+        void hDdisplay()const;
+        void vDisplay()const;
+        
+        // overloaded operators
+        /* Write your code here to declare/define the following overloaded operators:
+            - the stream insertion operator ( << )
+            - the relational operators (<, >, == )*/
+        friend std::ostream& operator<<(std::ostream &os, const College &college);
+        bool operator<(const College &college) { return code < college.code; }
+        bool operator>(const College &college) { return code > college.code; }
+        bool operator==(const College &college) { return code == college.code; }
+};
+
+#endif
